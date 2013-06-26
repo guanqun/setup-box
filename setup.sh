88ad3d0229b08af0323e64a3c40ad1c873529dc6
guanqun <guanqun.lu@gmail.com>
2013-06-26 22:44:28 +0800

    fix typo and relative position fix

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 # install ~/.gitconfig
 if [[ -f ./gitconfig/install ]]; then
-    ./gitconfig/install -f
+    cd gitconfig
+    ./install --force
+    cd ..
 fi
 
 # install ~/.vimrc
 cp vimrc ~/.vimrc
 
-if [[ "$SHELL" != "zsh" ]]; then
+if [[ "$SHELL" != "/bin/zsh" ]]; then
     ZSHRC="$HOME/.zshrc"
 
     echo "ZSH=`pwd`/oh-my-zsh" > $ZSHRC
     cat oh-my-zsh/templates/setup-box-zshrc >> $ZSHRC
-    cat aliases >> $ZSHRC
+    cat extras >> $ZSHRC
 
     chsh -s /bin/zsh
 fi
